7e02ae49aef0de5a7a443c6e9b59b6f217bf4e91
pisces2336 <pisces2336@gmail.com>
2024-11-25 02:11:06 +0900

    vuetify導入

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,18 @@
 import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(createVuetify({
+    components, directives,
+}))
 app.use(router)
 
 app.mount('#app')
