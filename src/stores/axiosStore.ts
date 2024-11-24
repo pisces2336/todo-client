e0d1b76a0bd7daaa68f6f1cb91d9cef3d041c329
pisces2336 <pisces2336@gmail.com>
2024-11-25 03:39:24 +0900

    sessionStoreの整理

--- src/stores/axiosStore.ts
+++ src/stores/axiosStore.ts
@@ -4,8 +4,21 @@ import { useCookies } from 'vue3-cookies'
 
 export const useAxiosStore = defineStore('axios', {
   actions: {
+    async get(url: string) {
+      const { cookies } = useCookies()
+
+      const accessToken = cookies.get('accessToken')
+      const config = { headers: { Authorization: `JWT ${accessToken}` } }
+      try {
+        const res = await axios.get(url, config)
+        return res
+      } catch {
+        return null
+      }
+    },
     async post(url: string, data: object) {
       const { cookies } = useCookies()
+
       const accessToken = cookies.get('accessToken')
       const config = { headers: { Authorization: `JWT ${accessToken}` } }
       try {
--- src/stores/sessionStore.ts
+++ src/stores/sessionStore.ts
@@ -1,10 +1,31 @@
+import type { User } from '@/types/api'
 import { defineStore } from 'pinia'
 import { useCookies } from 'vue3-cookies'
 import { useAxiosStore } from './axiosStore'
 
 export const useSessionStore = defineStore('session', {
+  state: () => ({
+    user: null as User | null,
+  }),
   actions: {
+    /**
+     * アクセストークンの取得とログインユーザーの取得を行う
+     */
     async login(username: string, password: string) {
+      return (await this._fetchAccessToken(username, password)) && (await this._fetchLoginUser())
+    },
+
+    /**
+     * アクセストークンの検証とログインユーザーの取得を行う
+     */
+    async isAuthenticated() {
+      return (await this._verifyAccessToken()) && (await this._fetchLoginUser())
+    },
+
+    /**
+     * アクセストークンを取得する
+     */
+    async _fetchAccessToken(username: string, password: string) {
       const axiosStore = useAxiosStore()
       const { cookies } = useCookies()
 
@@ -12,12 +33,43 @@ export const useSessionStore = defineStore('session', {
       const data = { username, password }
       const res = await axiosStore.post(url, data)
       if (!res) {
-        alert('ログインに失敗しました')
+        console.error('アクセストークンの取得に失敗しました')
         return false
       }
-
       cookies.set('accessToken', res.data.access)
-      alert('ログインに成功しました')
+      return true
+    },
+
+    /**
+     * アクセストークンを検証する
+     */
+    async _verifyAccessToken() {
+      const axiosStore = useAxiosStore()
+      const { cookies } = useCookies()
+
+      const url = '/api/auth/jwt/verify'
+      const data = { token: cookies.get('accessToken') }
+      const res = axiosStore.post(url, data)
+      if (!res) {
+        console.error('アクセストークンの検証に失敗しました')
+        return false
+      }
+      return true
+    },
+
+    /**
+     * ログインユーザーを取得する
+     */
+    async _fetchLoginUser() {
+      const axiosStore = useAxiosStore()
+
+      const url = '/api/auth/users/me'
+      const res = await axiosStore.get(url)
+      if (!res) {
+        console.error('ログインユーザーの取得に失敗しました')
+        return false
+      }
+      this.$state.user = res.data
       return true
     },
   },
